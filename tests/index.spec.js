93ef921cef09de21a55c3cde8c770d665733e3ae
mp40 <philps.matthew@gmail.com>
2018-11-16 15:49:03 +0900

    test environment set up

---
+++ tests/index.spec.js
@@ -0,0 +1,18 @@
+const {
+    expect
+} = require('chai')
+
+const {
+    describe,
+    it
+} = require('mocha')
+
+const {
+    test
+} = require('..')
+
+describe('tests', () => {
+    it('should prove test environment is set up', () => {
+        expect(test()).to.equal('working')
+    })
+})
