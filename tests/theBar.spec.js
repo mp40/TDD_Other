7d91982df90319e5371a963963dc8f829bd1ad1b
Matthew Philps <philps.matthew@gmail.com>
2019-01-14 14:57:38 +0900

    refacting of tests and code

--- tests/theBar.spec.js
+++ tests/theBar.spec.js
@@ -4,7 +4,8 @@ const {
 
 const {
   describe,
-  it
+  it,
+  xit
 } = require('mocha')
 
 const {
@@ -21,46 +22,22 @@ describe('tests', () => {
   })
 })
 
-describe('The bar', () => {
-  it('should have a fridge', () => {
-    expect(bar).to.contain.keys('fridge')
-  })
-  it('should have Asahi and Yebisu in the fridge', () => {
+describe('the bar', () => {
+  it('should have a fridge containing Asahi and Yebisu beers', () => {
     expect(bar.fridge).to.contain.keys('asahi', 'yebisu')
-  })
-  it('should have prices for beers', () => {
-    expect(bar.fridge.asahi).to.contain.keys('price')
-    expect(bar.fridge.asahi.price).to.be.a('number')
-    expect(bar.fridge.yebisu).to.contain.keys('price')
-    expect(bar.fridge.yebisu.price).to.be.a('number')
-  })
-  it('should have tally for beers', () => {
-    // expect(bar.fridge.asahi).to.contain.keys('price')
-    expect(bar.fridge.asahi.amount).to.be.a('number')
-    // expect(bar.fridge.yebisu).to.contain.keys('price')
-    expect(bar.fridge.yebisu.amount).to.be.a('number')
-  })
-  it('should keep track of takings', () => {
-    expect(bar.takings).to.be.a('number')
-  })
-  it('should have a closing time of midnight', () => {
-    expect(bar.closingTime).to.equal(12)
   })
 })
 
-describe('customers', () => {
-  const testCustomer = new Customer(1000, 2)
-  it('should have a Customer Class', () => {
-    expect(testCustomer).to.be.a('object')
-  })
+describe('customer properties', () => {
+  const customer = new Customer(1000, 2)
   it('should have a wallet for yen', () => {
-    expect(testCustomer.wallet).to.equal(1000)
+    expect(customer.wallet).to.equal(1000)
   })
   it('should have stamina defining how many hours they want to drink', () => {
-    expect(testCustomer.stamina).to.equal(2)
+    expect(customer.stamina).to.equal(2)
   })
   it('should have a default favourite beer of Asahi', () => {
-    expect(testCustomer.favourite).to.equal('asahi')
+    expect(customer.favourite).to.equal('asahi')
   })
   it('should be possible to change favorite beer', () => {
     const yebisuCustomer = new Customer(1000, 2, 'yebisu')
@@ -68,19 +45,34 @@ describe('customers', () => {
   })
 })
 
-describe('the customer at the bar', () => {
-  const newCustomer = new Customer(1000, 2)
-  buyDrink(newCustomer)
-  it('should be possible to find the customer a beer', () => {
-    expect(findDrink(newCustomer)).to.be.an('object').that.contains.keys('price', 'amount')
+describe('the customer drinking at the bar', () => {
+  describe('finding the right beer', () => {
+    it('should be possible to find their favourite beer', () => {
+      const newCustomer = new Customer(1000, 2)
+      const drink = findDrink(newCustomer)
+      expect(drink.price).to.equal(500)
+    })
+    it('should return undefined if their favourite beer is not stocked', () => {
+      const customer = new Customer(1000, 2, 'kirin')
+      const drink = findDrink(customer)
+      expect(drink).to.equal(undefined)
+    })
+    xit('should return out of beer message if amount is zero', () => {
+
+    })
   })
-  it('should be possible for the customer to buy a beer', () => {
-    expect(newCustomer.wallet).to.equal(500)
-  })
-  it('should remove a beer from the fridge when the customer buys it', () => {
-    expect(bar.fridge.asahi.amount).to.equal(11)
-  })
-  it('should add money to the takings when beer is purchased', () => {
-    expect(bar.takings).to.equal(500)
+  describe('buying a beer', () => {
+    const newCustomer = new Customer(1000, 2)
+    buyDrink(newCustomer)
+    it('should cost money to buy a beer', () => {
+      expect(newCustomer.wallet).to.equal(500)
+    })
+    it('should remove a beer from the fridge when the customer buys it', () => {
+      expect(bar.fridge.asahi.amount).to.equal(11)
+    })
+    // Test for edge case - running out of beer
+    it('should add money to the takings when beer is purchased', () => {
+      expect(bar.takings).to.equal(500)
+    })
   })
 })
--- theBar.js
+++ theBar.js
@@ -2,22 +2,26 @@ const test = function () {
   return 'working'
 }
 
-const bar = {
-  fridge: {
-    asahi: {
-      type: 'beer',
-      price: 500,
-      amount: 12
+const createBar = function () {
+  return {
+    fridge: {
+      asahi: {
+        type: 'beer',
+        price: 500,
+        amount: 12
+      },
+      yebisu: {
+        type: 'fancy beer',
+        price: 700,
+        amount: 6
+      }
     },
-    yebisu: {
-      type: 'fancy beer',
-      price: 700,
-      amount: 6
-    }
-  },
-  takings: 0,
-  closingTime: 12
+    takings: 0,
+    closingTime: 12
+  }
 }
+
+const bar = createBar()
 
 class Customer {
   constructor (wallet, stamina, favourite = 'asahi') {
