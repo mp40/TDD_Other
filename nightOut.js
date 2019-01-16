244cc7d2cce6611fb77ac5c882d01e372460eef6
Matthew Philps <philps.matthew@gmail.com>
2019-01-16 09:23:28 +0900

    Refactored nightOut

--- nightOut.js
+++ nightOut.js
@@ -5,7 +5,6 @@ const {
 } = require('.')
 
 const printBar = function () {
-  console.log('Start')
   console.log('The Fridge', matsBar.fridge)
   console.log('Takings', matsBar.takings)
   console.log("Richard's wallet", richard.wallet)
@@ -13,6 +12,7 @@ const printBar = function () {
 
 const matsBar = createBar()
 const richard = new Customer(6000, 4, 2)
-drinkBeer(matsBar, richard, 6)
-console.log('????')
+
+drinkBeer(richard, matsBar, 6)
+
 printBar()
